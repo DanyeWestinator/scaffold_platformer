fc962e2b6057de321bdc80ea6c8b87d7a9e21661
Jerome <arkserius@yahoo.com>
2020-05-26 07:10:33 -0700

    added bouncepad and ladder

---
+++ Assets/Scripts/Jerome/BouncePad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+    public int force = 25;
+
+    void OnTriggerEnter2D(Collider2D who)
+    {
+        // Assuming all colliders for bounce zone are tagged
+        if (who.tag == "Player")
+        {
+            ///////////// Rigidbody 2D
+            Rigidbody2D player = who.gameObject.GetComponent<Rigidbody2D>();
+            player.velocity = new Vector2(0f, force);
+            //player.AddForce(new Vector2(0f, force));
+        }
+    }
+}
---
+++ Assets/Scripts/Jerome/Ladder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    public int speed = 6;
+    public float gravityOffset = 1.18f;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" && Input.GetKey(KeyCode.W))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+
+        }
+        else if (other.tag == "Player" && Input.GetKey(KeyCode.S))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        }
+        else if(other.tag == "Player" && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            // To offset gravity. Not dynamic so it will be off if player gravity is changed.
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, gravityOffset);
+        }
+    }
+}
