7f2d47ad673ada234bdf900b638909170caad119
Jerome <arkserius@yahoo.com>
2020-05-26 10:49:55 -0700

    Main Menu implemented

---
+++ Assets/Scripts/Jerome/MainMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuController : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("jay_scene");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
